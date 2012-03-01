e8abe3d31aede8afde152c571eedf95d32ff937b
cpuwhiz11 <paul_geromini@student.uml.edu>
2012-03-01 14:18:02 -0500

    Init commit

---
+++ src/voter/main/VoterActivity.java
@@ -0,0 +1,13 @@
+package voter.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VoterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
